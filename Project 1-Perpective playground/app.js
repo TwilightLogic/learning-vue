91c69a6dcbb665a96e3629c7ee8beaf49caafb97
TwilightLogic <twilightlogic@gmail.com>
2023-02-28 10:39:53 +0800

    finished project-perspectivePlayground

--- Project 1-Perpective playground/app.js
+++ Project 1-Perpective playground/app.js
@@ -1,5 +1,22 @@
 const vm = Vue.createApp({
   data() {
-    return {};
+    return {
+      perspectiveInput: 0,
+      rotateXInput: 0,
+      rotateYInput: 0,
+      rotateZInput: 0,
+    };
+  },
+  computed: {
+    changePerspective() {
+      return {
+        perspective: `${this.perspectiveInput}px`,
+      };
+    },
+    changeRotate() {
+      return {
+        transform: `rotateX(${this.rotateXInput}deg) rotateY(${this.rotateYInput}deg) rotateZ(${this.rotateZInput}deg)`,
+      };
+    },
   },
 }).mount('#app');
