664ed286b873c25209e03564b1e0bb3729860264
TwilightLogic <twilightlogic@gmail.com>
2023-03-08 14:42:05 +0800

    Learning props in vue

--- fundamentals/11-vue project/components/src/components/User.vue
+++ fundamentals/11-vue project/components/src/components/User.vue
@@ -7,7 +7,22 @@
 <script>
 export default {
   name: 'User',
-  props: ['age'],
+  // Validating props
+  props: {
+    age: {
+      type: Number,
+      // ⬇️ 如果一个组件需要用到User这个组件，需要传入一个Number类型的age
+      // required: true,
+
+      // 我们也可以为age设置一个初始值(📒：如果age的type是一个Object/Array，下面的default要设成function)
+      default: 20,
+
+      // 也可以自定义一个validator()
+      validator(value) {
+        return value < 130;
+      },
+    },
+  },
   // ⬇️ 从组件发出的事件数组，添加了`age-change`事件
   // 用emits：仅当我们需要对任何props的值进行改变时才需要此过程
   emits: ['age-change'],
