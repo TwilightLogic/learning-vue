91be1d6fb39e3e4af3210faa3202ca4b9c0e737a
TwilightLogic <twilightlogic@gmail.com>
2023-03-27 10:07:58 +0800

    Building quiz-app

---
+++ fundamentals/13-quiz-app/quiz/src/components/Question.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div class="bar"></div>
+      <div class="status">1 out of 3 questions answered</div>
+    </div>
+    <div
+      class="single-question"
+      v-for="question in questionsArr"
+      :key="question.q"
+    >
+      <div class="question">{{ question.q }}</div>
+      <div class="answers">
+        <div
+          class="answer"
+          v-for="answer in question.answers"
+          :key="answer.text"
+        >
+          {{ answer.text }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['questionsArr'],
+};
+</script>
