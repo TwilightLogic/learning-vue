40017138a75621e38e5311358d8fd143c86920f9
TwilightLogic <twilightlogic@gmail.com>
2023-03-30 11:14:39 +0800

    Working with state by pinia

--- fundamentals/14-music-app/music/src/components/AppHeader.vue
+++ fundamentals/14-music-app/music/src/components/AppHeader.vue
@@ -9,8 +9,12 @@
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <!-- 当用户点击Login / Register时，应该跳转页面 -->
+          <!-- 如果isOpen是真值，那就跳转页面 -->
           <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+              >Login / Register</a
+            >
           </li>
           <li>
             <a class="px-2 text-white" href="#">Manage</a>
@@ -22,7 +26,23 @@
 </template>
 
 <script>
+import { mapStores } from 'pinia'
+import useModalStore from '@/stores/modal'
+
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  computed: {
+    // mapStores函数接受modal.js里的数据
+    ...mapStores(useModalStore)
+  },
+  methods: {
+    toggleAuthModal() {
+      // 这个时候我们要访问modal.js的isOpen的值，那我们要怎么访问呢？
+      // 用pinia自带的`mapStores`函数
+      // 为什么是this.modalStore?????
+      this.modalStore.isOpen = !this.modalStore.isOpen
+      console.log(this.modalStore.isOpen)
+    }
+  }
 }
 </script>
