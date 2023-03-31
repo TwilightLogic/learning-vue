a955edaee94c713913e01d61d8b033b0b980a142
TwilightLogic <twilightlogic@gmail.com>
2023-03-31 11:05:35 +0800

    build register and login tabs

--- fundamentals/14-music-app/music/src/components/AppAuth.vue
+++ fundamentals/14-music-app/music/src/components/AppAuth.vue
@@ -29,18 +29,32 @@
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
               <a
-                class="block rounded py-3 px-4 transition hover:text-white text-white bg-blue-600"
+                class="block rounded py-3 px-4 transition"
                 href="#"
+                @click.prevent="tab = 'login'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'login',
+                  'hover:text-blue-600': tab === 'register'
+                }"
                 >Login</a
               >
             </li>
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" href="#">Register</a>
+              <a
+                class="block rounded py-3 px-4 transition"
+                href="#"
+                @click.prevent="tab = 'register'"
+                :class="{
+                  'hover:text-white text-white bg-blue-600': tab === 'register',
+                  'hover:text-blue-600': tab === 'login'
+                }"
+                >Register</a
+              >
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab === 'login'">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -67,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -147,6 +161,13 @@ import useModalStore from '@/stores/modal'
 
 export default {
   name: 'AppAuth',
+  // 这里用data是为了存储tab(register / login)的信息
+  // 我们会根据tab的不同来渲染页面
+  data() {
+    return {
+      tab: 'login'
+    }
+  },
   // getters要用computed来接收
   computed: {
     // 用mapWritableState来关闭这个login/register modal
