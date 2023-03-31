fb41f134022348aa381f9f8837f2ebb5fcc16ec0
TwilightLogic <twilightlogic@gmail.com>
2023-03-31 10:02:40 +0800

    Using getters of pinia to pass state and build a hidden-class event (Login / Register page)

--- fundamentals/14-music-app/music/src/components/AppAuth.vue
+++ fundamentals/14-music-app/music/src/components/AppAuth.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Auth Modal -->
-  <div class="fixed z-10 inset-0 overflow-y-auto hidden" id="modal">
+  <div class="fixed z-10 inset-0 overflow-y-auto" id="modal" :class="hiddenClass">
     <div
       class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
@@ -142,7 +142,14 @@
 </template>
 
 <script>
+import { mapStores } from 'pinia'
+import useModalStore from '@/stores/modal'
+
 export default {
-  name: 'AppAuth'
+  name: 'AppAuth',
+  // getters要用computed来接收
+  computed: {
+    ...mapStores(useModalStore, ['hiddenClass'])
+  }
 }
 </script>
--- fundamentals/14-music-app/music/src/stores/modal.js
+++ fundamentals/14-music-app/music/src/stores/modal.js
@@ -1,8 +1,14 @@
 import { defineStore } from 'pinia'
 
 export default defineStore('modal', {
+  // 相当于 state: () => { return { isOpen: false } }
   state: () => ({
     isOpen: false
-  })
-  // ⬆️ 相当于 state: () => { return { isOpen: false } }
+  }),
+  getters: {
+    // If isOpen is true, the class should be applied to element
+    hiddenClass(state) {
+      return !state.isOpen ? 'hidden' : ''
+    }
+  }
 })
