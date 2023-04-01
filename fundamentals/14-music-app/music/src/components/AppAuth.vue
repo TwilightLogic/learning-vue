349a19f2559314209bc6129b8ae17a9d7b3fbcfb
TwilightLogic <twilightlogic@gmail.com>
2023-04-01 16:50:47 +0800

    set USA as default country in register tab

--- fundamentals/14-music-app/music/src/components/AppAuth.vue
+++ fundamentals/14-music-app/music/src/components/AppAuth.vue
@@ -81,7 +81,12 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="userData"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -202,6 +207,9 @@ export default {
         confirm_password: 'confirmed:@password',
         country: 'required|not_one_of:Antarctica',
         tos: 'required'
+      },
+      userData: {
+        country: 'USA'
       }
     }
   },
