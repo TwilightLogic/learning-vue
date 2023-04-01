9162f6cd2a0a3140a5695fb8ae11adad96b8d43c
TwilightLogic <twilightlogic@gmail.com>
2023-04-01 11:47:48 +0800

    added rules to password and confirm_password propepty in register tab

--- fundamentals/14-music-app/music/src/components/AppAuth.vue
+++ fundamentals/14-music-app/music/src/components/AppAuth.vue
@@ -118,20 +118,24 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
+                name="password"
                 type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input
+              <vee-field
+                name="confirm_password"
                 type="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password"
               />
+              <ErrorMessage class="text-red-600" name="confirm_password" />
             </div>
             <!-- Country -->
             <div class="mb-3">
@@ -178,8 +182,9 @@ export default {
         name: 'required|min:3|max:10|alpha_spaces',
         email: 'required|min:3|max:100|email',
         age: 'required|min_value:18|max_value:100',
-        password: '',
-        confirm_password: '',
+        password: 'required|min:3|max:100',
+        // confirmed: @password 指定了password这个name的值要和confirm_password这个name的值相同
+        confirm_password: 'confirmed:@password',
         country: '',
         tos: ''
       }
