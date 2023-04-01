e668ff3525efe42c7517dfe3512c9d1100d3ab3e
TwilightLogic <twilightlogic@gmail.com>
2023-04-01 15:52:59 +0800

    added submit event and validate the form

--- fundamentals/14-music-app/music/src/components/AppAuth.vue
+++ fundamentals/14-music-app/music/src/components/AppAuth.vue
@@ -81,7 +81,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form v-show="tab === 'register'" :validation-schema="schema" @submit="register">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -211,6 +211,11 @@ export default {
       modalVisibility: 'isOpen'
     })
   },
-  components: { ErrorMessage }
+  components: { ErrorMessage },
+  methods: {
+    register(values) {
+      console.log(values)
+    }
+  }
 }
 </script>
