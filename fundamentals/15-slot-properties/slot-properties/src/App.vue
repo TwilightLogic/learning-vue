c897518ea6b6b8746cf02b437ac0c5cb5bb13ccd
TwilightLogic <twilightlogic@gmail.com>
2023-04-01 16:27:09 +0800

    learning slot properties

---
+++ fundamentals/15-slot-properties/slot-properties/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- 如果App要用HelloWorld给的数据，这里就要用v-slot，并且注册一个别名：v `v-slot="v"` -->
+    <!-- 新版本：可以用#来代替v-slot，然后在#后面写个default，别名也省略了 -->
+    <hello-world #default="{ user, favorites }">
+      <p>Hello {{ user.name }}. You like {{ favorites[0] }}, right?</p>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ fundamentals/15-slot-properties/slot-properties/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- 这里注册了一个user属性，App就能调用HelloWorld里面的数据了 -->
+    <slot :user="user" :favorites="favorites"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      user: {
+        name: 'John',
+        age: 25,
+      },
+      favorites: ['Pizza', 'Marbles', 'Birds'],
+    };
+  },
+};
+</script>
