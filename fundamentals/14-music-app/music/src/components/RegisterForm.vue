b38777d18db9241f0599ffa45ebd05efc034f5c7
TwilightLogic <twilightlogic@gmail.com>
2023-04-06 15:13:05 +0800

    Using firebase to save the data and validate the data we have registered

--- fundamentals/14-music-app/music/src/components/RegisterForm.vue
+++ fundamentals/14-music-app/music/src/components/RegisterForm.vue
@@ -144,17 +144,27 @@ export default {
 
       // 我们已经在上面添加了alert的数据
       // 但是这里我们希望使用firebase发送数据 ⬇️
-      // 返回一个对象，其中包含我们可以用来与身份验证进行通信的方法和属性
-      const userCred = await firebase
-        .auth()
-        .createUserWithEmailAndPassword(values.email, values.password)
+      // firebase.auth().createUserWithEmailAndPassword()返回一个对象，其中包含我们可以用来与身份验证进行通信的方法和属性
+      let userCred = null
+      try {
+        // firebase: User account creation can fail if the account already exists or the password is invalid.
+        // ⬆️ 所以如果我们注册同样的账号，会直接导致异常
+        userCred = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(values.email, values.password)
+      } catch (error) {
+        this.reg_in_submission = false
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_msg = 'An unexpected error ocurred. Please try again later.'
 
-      console.log(userCred)
+        // 这个return函数会停止函数的继续执行
+        return
+      }
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
 
-      console.log(values)
+      console.log(userCred)
     }
   }
 }
