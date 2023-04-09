12d133a12d599fb7b8db2fcc6a62a707da8fa881
TwilightLogic <twilightlogic@gmail.com>
2023-04-09 22:07:50 +0800

    Initializing firebase first

--- fundamentals/14-music-app/music/src/main.js
+++ fundamentals/14-music-app/music/src/main.js
@@ -4,15 +4,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './includes/validation'
+import { auth } from './includes/firebase'
 
 import './assets/base.css'
 import './assets/main.css'
 
-const app = createApp(App)
+let app
 
-// `use` methods allow us to register the plugins
-app.use(createPinia())
-app.use(router)
-app.use(VeeValidatePlugin)
+// We've loaded Firebase so that we can check if the user is authenticated when the application starts ⬇️
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App)
 
-app.mount('#app')
+    // `use` methods allow us to register the plugins
+    app.use(createPinia())
+    app.use(router)
+    app.use(VeeValidatePlugin)
+
+    app.mount('#app')
+  }
+})
