a28a485491eb8ecd1c40b7837f36350d0d3fd2d2
TwilightLogic <twilightlogic@gmail.com>
2023-04-09 22:29:29 +0800

    Persisting the user authentication

--- fundamentals/14-music-app/music/src/App.vue
+++ fundamentals/14-music-app/music/src/App.vue
@@ -273,12 +273,21 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
 import AppAuth from './components/AppAuth.vue'
+import { mapWritableState } from 'pinia'
+import useUserStore from '@/stores/user'
+import { auth } from './includes/firebase'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     AppAuth
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ['userLoggedIn'])
+  },
+  created() {
+    auth.currentUser ? (this.userLoggedIn = true) : ''
   }
 }
 </script>
