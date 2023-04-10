82c9da31225b0c1ce0482d740fcd426ff9132baf
TwilightLogic <twilightlogic@gmail.com>
2023-04-10 15:25:10 +0800

    Add `logout` item in navigation bar when the userLoggedIn is true

--- fundamentals/14-music-app/music/src/components/AppHeader.vue
+++ fundamentals/14-music-app/music/src/components/AppHeader.vue
@@ -11,14 +11,20 @@
           <!-- Navigation Links -->
           <!-- 当用户点击Login / Register时，应该跳转页面 -->
           <!-- 如果isOpen是真值，那就跳转页面 -->
-          <li>
+          <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
             >
           </li>
-          <li>
-            <a class="px-2 text-white" href="#">Manage</a>
-          </li>
+          <!-- 因为没办法添加多个v-else，所以给他们一个template标签就好了 -->
+          <template v-else>
+            <li>
+              <a class="px-2 text-white" href="#">Manage</a>
+            </li>
+            <li>
+              <a class="px-2 text-white" href="#">Logout</a>
+            </li>
+          </template>
         </ul>
       </div>
     </nav>
@@ -28,12 +34,14 @@
 <script>
 import { mapStores } from 'pinia'
 import useModalStore from '@/stores/modal'
+// 这里导入一个user.js来查看用户登录状态
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'AppHeader',
   computed: {
     // mapStores函数接受modal.js里的数据
-    ...mapStores(useModalStore)
+    ...mapStores(useModalStore, useUserStore)
   },
   methods: {
     toggleAuthModal() {
--- fundamentals/14-music-app/music/src/components/RegisterForm.vue
+++ fundamentals/14-music-app/music/src/components/RegisterForm.vue
@@ -203,6 +203,8 @@ export default {
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
 
+      window.location.reload()
+
       // console.log(userCred)
     }
   }
